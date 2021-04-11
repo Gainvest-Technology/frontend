0dfbed52c35dd3f8529eb5bf1e23098efb8c14e6
Todd Lee <toddtlee48@gmail.com>
2021-04-11 16:51:25 -0400

    ios and android

---
+++ ios/App/public/static/js/53.b54be72a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgainvest-io"]=this["webpackJsonpgainvest-io"]||[]).push([[53],{630:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return o}));var i=e(27),o=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.j)((function(){var t=n.innerWidth,e=n.innerHeight,o=document.elementFromPoint(t/2,e/2);if(o){var c=o.closest("ion-content");c&&c.componentOnReady().then((function(){Object(i.g)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.b54be72a.chunk.js.map
